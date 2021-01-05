7999f9b758f3d3d6cc480a25433589c9f77279ed
El George <jorgeesparza@hotmail.com>
2021-01-05 00:26:22 -0600

    Se implemento el ptron de diseño modulo, para restringir el acceso de las propiedades y elementos del juego, por lo que en resumidas cuentas se hizo una funicion auto-autoinvocada para que dentro de esta funcion este el juego, este es una forma de programar la cual conciste en no declarar una clase como tal

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -5,134 +5,170 @@
  * s4 -> four of swords
  */
 
-let deck = []; //mazo de cartas
-const tipos = ['c','d','h','s'];//Tipos de cartas
-let puntosJugador = 0,
-    puntosComputadora = 0;
+//  Funcion anonima autoinvocada, consciste en que la funcion anonima se va auto llamar en tiempo de ejecucion
+// Esta funcion sirve para realizar el patron de diseño modulo
+const modulo = (() => {
 
-// Referencias de html
+    'use strict'; // instruccion que indica al interprete de JS que sea 'estricto' al momento de ejecutar, mostrara mas errores en pocas palabras  
+    
 
-const btnPedir = document.querySelector( '#btnPedir' );
-const btnDetener = document.querySelector( '#btnDetener' );
-const btnNuevo = document.querySelector( '#btnNuevo' );
+     let deck = []; //mazo de cartas
+     const tipos = ['c', 'd', 'h', 's'];//Tipos de cartas
 
-const puntosHTML = document.querySelectorAll('small');
+    //                      
+     let puntosJugadores = []; // la ultima posicion del arreglo se le asigna a la computadora, las primeras posiciones son para los jugadores
 
-const divCartasJugador = document.querySelector('#jugador-cartas');
-const divCartasComputadora = document.querySelector('#computadora-cartas');
+     // Referencias de html
+
+    const btnPedir = document.querySelector('#btnPedir'),
+        btnDetener = document.querySelector('#btnDetener'),
+        btnNuevo = document.querySelector('#btnNuevo');
+
+    const divCartasJugadores = document.querySelectorAll('.divCartas'),
+        puntosHTML = document.querySelectorAll('small');
 
 
+    // funcion que inicializa el juego
+     const inicializarJuego = ( numJugadores = 2 ) => {
 
-//funcion que crea el mazo
-const crearDeck = () => {
+        deck = crearDeck();
 
-    // Se llena el arreglo deck (mazo de cartas)
-    for( let i = 1 ; i <= 13 ; i++ ){
-        for (let tipo of tipos) {
-            deck.push(tipo + i);    
+        puntosJugadores = []; // Se reinicializa todos los jugadores, por lo que se vacia el  deck
+
+        for( let i = 0 ; i < numJugadores ; i++){
+            puntosJugadores.push(0);
         }
 
+        puntosHTML.forEach(elem => elem.innerText = 0 );// se inicializa en el html en 0 en cada jugador y computadora
+
+        divCartasJugadores.forEach(elem => elem.innerText = '' );//
+
+        btnPedir.disabled = false;
+        btnDetener.disabled = false;
+
+     }
+
+     //funcion que crea el mazo
+     const crearDeck = () => {
+
+        deck = [];
+
+         // Se llena el arreglo deck (mazo de cartas)
+         for (let i = 1; i <= 13; i++) {
+             for (let tipo of tipos) {
+                 deck.push(tipo + i);
+             }
+         }
+
+         return _.shuffle(deck);
+     }
+     
+
+     // Funcion que permite tomar una carta
+     const pedirCarta = () => ((deck.length === 0) ? 'lo siento ya no hay cartas' : deck.pop());
+
+
+     //Se extrae el ultimo valor de la cadena, ejemplo: 'd12' -> '12' -> 12
+     const trasnformarValor = (carta) => carta.substring(1, carta.length) * 1;
+
+
+     // Funcion que sirve para obtener el valor de una carta
+     const valorCarta = (carta) => (trasnformarValor(carta) === 1) ? 11 : (trasnformarValor(carta) < 10) ? trasnformarValor(carta) : 10;
+
+    //  Turno: 0 => primer juagdor y el ultimo => compu
+     const acumularPuntos = ( carta , turno ) => {
+
+        puntosJugadores[turno] += valorCarta(carta);//se obtiene los puntos de la carta y se van sumando en esta variable
+        puntosHTML[turno].innerHTML = puntosJugadores[turno]; //Se modifica el html en la parte donde dice  <small>aquie va el numero que cambia</small>
+
+        return puntosJugadores[turno];
+
+     }
+
+    const cartaNueva = (carta, turno) => {
+
+        const imgCarta = document.createElement('img');// se crea el elemneto de tipo imagen y se agrega al HTML
+        imgCarta.src = `assets/Cartas/${carta}.png`;// se le da como atributo la imagen en cuestio, la cual depende de la carta que se pidio
+        imgCarta.classList.add('carta');// se le da la clase de estilo de bootstrap la cual nombramos como carta
+        divCartasJugadores[turno].append(imgCarta);
+
     }
 
-    
-    deck = _.shuffle(deck);
+    const determinarGanador = () => {
+        
+        const [ puntosMinimos , puntosComputadora ] = puntosJugadores;
 
-    return deck;
-}
-
-crearDeck();
-
-// Funcion que permite tomar una carta
-const pedirCarta = () => ((deck.length===0) ? 'lo siento ya no hay cartas' : deck.pop());
-
-
-//Se extrae el ultimo valor de la cadena, ejemplo: 'd12' -> '12' -> 12
-const trasnformarValor = ( carta ) => carta.substring(1,carta.length)*1;
-
-
-// Funcion que sirve para obtener el valor de una carta
-const valorCarta = ( carta ) => (trasnformarValor(carta) === 1 ) ? 11 : (trasnformarValor(carta) < 10) ? trasnformarValor(carta) : 10;
-
-const turnoComputadora = ( puntosMinimos ) => {
-
-    do{
-
-        const carta = pedirCarta(); // se pide como tal una carta
-        puntosComputadora += valorCarta( carta );//se obtiene los puntos de la carta y se van sumando en esta variable
-        puntosHTML[1].innerHTML = puntosComputadora; //Se modifica el html en la parte donde dice  <small>aquie va el numero que cambia</small>
-        divCartasComputadora.append( cartaNueva(carta) );//Se agrega una nueva carta
-
-        if( puntosMinimos > 21) break;
-    
-    }while( ( puntosComputadora < puntosMinimos ) && ( puntosMinimos <= 21 ));
-
-    // setTimeout sirve para ejecutar la instruccion que lleva dentro de la funcion anonima depues de un tiempo en milisegundos, este caso es 10 ms.
-    // Ahora bien, se usa setTimeout en esta caso por que en javascript todo es por hilos y se procesa de manera simultaneo, y con esta instruccion lo que se hace es esperar a que termine el hilo del ciclo do-while 
-    setTimeout(() => alert( ( (puntosComputadora === puntosMinimos) ? 'Nadie gana' : (puntosMinimos > 21) ? 'Computadora gana' : (puntosComputadora > 21) ? 'Jugador gana' : 'Computadora gana') ), 30);
-
-}
-
-const cartaNueva = ( carta ) => {
-
-    const imgCarta = document.createElement('img');// se crea el elemneto de tipo imagen y se agrega al HTML
-    imgCarta.src = `assets/Cartas/${ carta }.png`;// se le da como atributo la imagen en cuestio, la cual depende de la carta que se pidio
-    imgCarta.classList.add('carta');// se le da la clase de estilo de bootstrap la cual nombramos como carta
-    return imgCarta; // se regresa el objeto como tal
-
-}
-
-// EVENTOS
-
-// Boton pedir carta: al hacer click agrega cartas al jugador y dependiendo de la suma total de puntos, se deshabilitan los botones de pedir cartas y detener
-btnPedir.addEventListener('click',()=>{
-
-    // aqui va el callback 
-    const carta = pedirCarta(); // se pide como tal una carta
-    puntosJugador += valorCarta( carta );//se obtiene los puntos de la carta y se van sumando en esta variable
-    puntosHTML[0].innerHTML = puntosJugador; //Se modifica el html en la parte donde dice  <small>aquie va el numero que cambia</small>
-    divCartasJugador.append( cartaNueva(carta) );//Se agrega una nueva carta
-
-    if( puntosJugador > 21 ){//si el jugador obtiene mas de 21 puntos, pierde y se deshabilita el boton de pedir carta
-
-        btnPedir.disabled = true;
-        btnDetener.disabled = true;
-        console.warn( 'Lo siento, perdiste mi crack' );
-        turnoComputadora( puntosJugador );
-
-    }else if ( puntosJugador === 21 ){//si el jugador obtiene 21 puntos, gana por lo que se deshabilita el boton de pedir carta
-
-        btnPedir.disabled = true;
-        console.warn('21, ahuevo crack');
-        turnoComputadora( puntosJugador );
+        // setTimeout sirve para ejecutar la instruccion que lleva dentro de la funcion anonima depues de un tiempo en milisegundos, este caso es 10 ms.
+        // Ahora bien, se usa setTimeout en esta caso por que en javascript todo es por hilos y se procesa de manera simultaneo, y con esta instruccion lo que se hace es esperar a que termine el hilo del ciclo do-while 
+        setTimeout(() => alert(((puntosComputadora === puntosMinimos) ? 'Nadie gana' : (puntosMinimos > 21) ? 'Computadora gana' : (puntosComputadora > 21) ? 'Jugador gana' : 'Computadora gana')), 30);
     }
 
-});
+     const turnoComputadora = (puntosMinimos) => {
 
-// Boton detener: este funciona para pasar el turno a la computadora, cuando ya no se quiere pedir cartas 
-btnDetener.addEventListener('click',()=>{
+        let puntosComputadora = 0;
 
-    btnPedir.disabled = true;
-    btnDetener.disabled = true;
-    turnoComputadora( puntosJugador );//Se pasa el turno a la computadora
+         do {
 
-});
+            const carta = pedirCarta(); // se pide como tal una carta
+            puntosComputadora = acumularPuntos( carta , puntosJugadores.length-1 );
+            cartaNueva(carta , puntosJugadores.length-1);
 
-// Boton Nuevo Juego: reinicia el programa practicamente, realiza un nuevo deck y vacia el espacio del jugador y de la computadora, ademas, se inicializa en 0 los puntajes
-btnNuevo.addEventListener('click', () => {
-    
-    console.clear();
-    deck = [];
-    deck = crearDeck();
+            if (puntosMinimos > 21) break;
 
-    puntosComputadora = 0;
-    puntosJugador = 0;
+         } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
 
-    puntosHTML[0].innerText = puntosJugador;
-    puntosHTML[1].innerText = puntosComputadora;
+         determinarGanador();
 
-    divCartasJugador.innerHTML = '';
-    divCartasComputadora.innerHTML = '';
+     }
 
-    btnPedir.disabled = false;
-    btnDetener.disabled = false;
-});
+     // EVENTOS
+
+     // Boton pedir carta: al hacer click agrega cartas al jugador y dependiendo de la suma total de puntos, se deshabilitan los botones de pedir cartas y detener
+     btnPedir.addEventListener('click', () => {
+
+         // aqui va el callback 
+         const carta = pedirCarta(); // se pide como tal una carta
+         const puntosJugador = acumularPuntos(carta, 0);
+
+         cartaNueva( carta, 0 );//Se agrega una nueva carta
+
+         if (puntosJugador > 21) {//si el jugador obtiene mas de 21 puntos, pierde y se deshabilita el boton de pedir carta
+
+             btnPedir.disabled = true;
+             btnDetener.disabled = true;
+             console.warn('Lo siento, perdiste mi crack');
+             turnoComputadora(puntosJugador);
+
+         } else if (puntosJugador === 21) {//si el jugador obtiene 21 puntos, gana por lo que se deshabilita el boton de pedir carta
+
+             btnPedir.disabled = true;
+             btnDetener.disabled = true;
+             console.warn('21, muy bien mi crack');
+             turnoComputadora(puntosJugador);
+         }
+
+     });
+
+     // Boton detener: este funciona para pasar el turno a la computadora, cuando ya no se quiere pedir cartas 
+     btnDetener.addEventListener('click', () => {
+
+         btnPedir.disabled = true;
+         btnDetener.disabled = true;
+
+         turnoComputadora(puntosJugadores[0]);//Se pasa el turno a la computadora
+
+     });
+
+    //  la funcion retorna un objeto el cual todos sus atributos, son las referencias  los cuales seran publicos, por lo tanto visible
+     return {
+
+        nuevoJuego: inicializarJuego
+
+     };
+
+ })();
+
+//  NOTA: el modo estricto es recomendable usar cuando se implementa el patron de deseño modulo
+
+
+
